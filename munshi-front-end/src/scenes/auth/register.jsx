4a5ff7a4c589b0be6d184a955ca2f72402d98d94
pypatel593m <pypatel593m@gmail.com>
2023-03-24 12:04:50 -0400

    register page fixed

--- munshi-front-end/src/scenes/auth/register.jsx
+++ munshi-front-end/src/scenes/auth/register.jsx
@@ -1,62 +1,48 @@
-import {Box, Button, TextField, useTheme, Select, MenuItem, Autocomplete } from "@mui/material"; 
+import {Box, Button, TextField, useTheme, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup } from "@mui/material"; 
 import React, { useState, useRef }  from 'react';
 import Header  from "../../components/Header";
 import Axios from "axios";
 import {useNavigate} from 'react-router-dom';
 import {User} from "../../models/users";
-import { Formik } from "formik";
+import { Formik, useFormik } from "formik";
 import * as yup from "yup";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import { border } from "@mui/system";
+import { pink, yellow } from "@mui/material/colors";
 
 const Register = () => {
   
   const navigate = useNavigate();
-  const [user_email_address, setEmailAddress] = useState("");
-  const [user_password, setPassword] = useState(""); 
-  const [user_fname, setFirstName] = useState("");
-  const [user_lname, setLastName] = useState("");
-  const [user_phone, setPhone] = useState("");
-  const [user_address, setAddress] = useState("");
-  const [user_type_id, setTypeID] = useState("1");
   const [user_business_id, setUserBusinessID] = useState("");
   
   const [registerStatus, setRegisterStatus] = useState("");
   const isNonMobile = useMediaQuery("(min-width:600px)");
-  const autocompleteRef = useRef();
 
   function handleFormSubmit(values)
   {
-    const selectedValue = values.user_type_id;
-    console.log(selectedValue);
-    console.log("Valuesssssssss", values.user_email_address, values.user_password, values.user_fname, values.user_lname, values.user_phone, values.user_address, values.user_type_id, values.user_business_id);
+    Axios.post("http://localhost:3001/register", {
+      user_email_address : values.user_email_address,
+      user_password : values.user_password,
+      user_fname : values.user_fname,
+      user_lname : values.user_lname,
+      user_phone : values.user_phone,
+      user_address : values.user_address,
+      user_type_id : values.user_type_id,
+      user_business_id : values.user_business_id,
 
-    // Axios.post("http://localhost:3001/register", {
-    //   user_email_address : values.user_email_address,
-    //   user_password : values.user_password,
-    //   user_fname : values.user_fname,
-    //   user_lname : values.user_lname,
-    //   user_phone : values.user_phone,
-    //   user_address : values.user_address,
-    //   user_type_id : values.user_type_id,
-    //   user_business_id : values.user_business_id,
-
-    // }).then((response) => {
-    //   if(response.data.message)
-    //   {
-    //     navigate("/login");
-    //   }
-    //   else
-    //   {
-    //     setRegisterStatus(response.data.message)
-    //   }
-    // });
+    }).then((response) => {
+      if(response.data.message)
+      {
+        
+        setRegisterStatus(response.data.message);
+      }
+      else
+      {
+        navigate('/login');
+      }
+    });
   }
 
-  const handleSelectChange = (event) => {
-    setTypeID(event.target.value);
-  };
-  const options = ['Employee', 'Employer'];
 
     return <Box m="20px">
         <Box display="flex" justifyContent={"center"}>
@@ -181,18 +167,20 @@ const Register = () => {
                     helperText={touched.user_business_id && errors.user_business_id}
                     sx={{ gridColumn: "span 2" }}
                   />
-                  <Select value={user_type_id} name="user_type_id" onChange={handleSelectChange}
-                     sx={{ gridColumn: "span 2" }} required>
-                    <MenuItem value="1">Employee</MenuItem>
-                    <MenuItem value="2">Employer</MenuItem>
-                  </Select>
-                  {/* <Autocomplete
-                    disablePortal
-                    name="user_type_id"
-                    options={options}
-                    sx={{ gridColumn: "span 2" }} ref={autocompleteRef}
-                    renderInput={(params) => <TextField {...params} label="User Type" />}
-                  /> */}
+                  <FormControl>
+                    <FormLabel id="demo-radio-buttons-group-label">Select Type</FormLabel>
+                    <RadioGroup
+                      row
+                      aria-labelledby="demo-radio-buttons-group-label"
+                      value={values.user_type_id}
+                      name="user_type_id"
+                      onChange={handleChange}
+                      required
+                    >
+                      <FormControlLabel value="2" control={<Radio />} label="Employee" />
+                      <FormControlLabel value="1" control={<Radio />} label="Employer" />
+                    </RadioGroup>
+                  </FormControl>
                   <Box display="flex" justifyContent="end" mt="20px">
                     <Button type="submit" color="secondary" variant="contained">
                       Register
@@ -211,53 +199,6 @@ const Register = () => {
             </Formik>
         </Box>
         
-        <Box className="loginForm">
-        {/* <form>
-        <h1 style={{color: 'red', fontSize: '15px', textAlign: 'center', marginTop: '20px'}} >{registerStatus}</h1>
-          <div className="container">
-            <label htmlFor="user_email_address">Email:</label>
-            <input type="email" className="textInput" name="user_email_address" onChange={(e) => {setEmailAddress(e.target.value)}} required placeholder="Enter your email"/>
-          </div>
-          <div className="container">
-            <label htmlFor="user_password">Password:</label>
-            <input type="password" className="textInput" name="user_password" onChange={(e) => {setPassword(e.target.value)}} required placeholder="Enter your password"/>
-          </div>
-          <div className="container">
-            <label htmlFor="user_fname">First Name:</label>
-            <input type="text" className="textInput" name="user_fname" onChange={(e) => {setFirstName(e.target.value)}} required placeholder="Enter your first name"/>
-          </div>
-          <div className="container">
-            <label htmlFor="user_lname">Last Name:</label>
-            <input type="text" className="textInput" name="user_lname" onChange={(e) => {setLastName(e.target.value)}} required placeholder="Enter your last name"/>
-          </div>
-          <div className="container">
-            <label htmlFor="user_phone">Phone:</label>
-            <input type="text" className="textInput" name="user_phone" onChange={(e) => {setPhone(e.target.value)}} required placeholder="Enter your phone number"/>
-          </div>
-          <div className="container">
-            <label htmlFor="user_address">Address:</label>
-            <input type="text" className="textInput" name="user_address" onChange={(e) => {setAddress(e.target.value)}} required placeholder="Enter your address"/>
-          </div>
-          <div className="container">
-            <label htmlFor="user_type_id">Select user type</label>
-            <select id="user_type_id" onChange={(e) => {setTypeID(e.target.value)}} name="user_type_id">
-              <option value="1">Employer</option>
-              <option value="2">Employee</option>
-            </select>
-          </div>
-          <div className="container">
-            <label htmlFor="user_business_id">Business ID:</label>
-            <input type="number" className="textInput" name="user_business_id" onChange={(e) => {setUserBusinessID(e.target.value)}} required placeholder="Enter your business id."/>
-          </div>
-          <div className="container">
-            <input className="button" type="submit" value="Register" onClick={handleRegister}/>
-          </div>
-          <p>
-            Already have an account? 
-            <a href="./login"> Sign in</a>
-          </p>
-        </form> */}
-        </Box>
     </Box>;
 }
 
@@ -294,7 +235,7 @@ const checkoutSchema = yup.object().shape({
   .matches(phoneRegExp, "Invalid Phone Number.")
   .required("required"),
   user_address: yup.string().required("required"),
-  user_type_id: yup.string().required("required"),
+  user_type_id: yup.string().required('Please select user type'),
   user_business_id: yup.string().required("required"),
 });
 const initialValues = {
@@ -304,6 +245,7 @@ const initialValues = {
   user_password: "",
   user_phone: "",
   user_address: "",
+  user_type_id: "Employee",
   user_business_id: "",
 };
 
