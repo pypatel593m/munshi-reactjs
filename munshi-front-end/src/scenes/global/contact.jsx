ff55e26e7a9c7bd5b84b1565bb3ff3d229b2c49c
pypatel593m <pypatel593m@gmail.com>
2023-03-27 18:46:33 -0400

    fixed the contact page

--- munshi-front-end/src/scenes/global/contact.jsx
+++ munshi-front-end/src/scenes/global/contact.jsx
@@ -4,6 +4,7 @@ import useMediaQuery from "@mui/material/useMediaQuery";
 import {useNavigate} from 'react-router-dom';
 import LocalPhoneRoundedIcon from '@mui/icons-material/LocalPhoneRounded';
 import AttachEmailIcon from '@mui/icons-material/AttachEmail';
+import PersonOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
@@ -21,92 +22,90 @@ const Contact = () => {
             <Box display="flex" justifyContent={"center"}>
                   <Box display="grid" justifyContent={"center"}>
                     <Box display={"flex"} justifyContent="center">
-                      <Header title="Contact!" subtitle="" />
+                      <Header title="Contact Us!" subtitle="" />
                     </Box>
                     <Box
                       display="grid"
-                      gap="30px"
                       justifyContent={"center"} 
+                      marginLeft={15}
                       gridTemplateColumns="repeat(8, minmax(0, 1fr))"
                       sx={{
                         "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
                       }}//sx={{ gridColumn: "span 2" }}
                     >
                       
-                      <Box sx={{ gridColumn: "span 8" }}>
-                      <section className="contact section" sx={{ gridColumn: "span 8" }}>
-                          <Box className="row">
-                                      <Box className="section-title padd-15">
-                                          <h2>Contact Me</h2>
-                                      </Box>
-                                  </Box>
-                                  <h3 className="contact-title padd-15">Have you any Questions?</h3>
-                                  <h4 className="contact-sub-title padd-15">I'm AT YOUR SERVICES</h4>
-                                  <Box className="row">
-                                      <Box className="contact-info-item padd-15">
-                                          <LocalPhoneRoundedIcon fontSize="large" sx={{ color: colors.redAccent[500] }}/>
-                                          <h4>Phone</h4>
-                                          <p>(905) 260-1008</p>
-                                      </Box>
-                                      <Box className="contact-info-item padd-15">
-                                          <AttachEmailIcon fontSize="large" sx={{ color: colors.redAccent[500] }}/>
-                                          <h4>Email</h4>
-                                          <p>pateldhruv9825@gmail.com</p>
-                                      </Box>
-                                  </Box>
-                                  <h3 className="contact-title padd-15">Send me and Email</h3>
-                                  <h4 className="contact-sub-title padd-15">I'm Very Responsive to Messages</h4>
-                                  <Box className="row">
-                                      <Box className="contact-form padd-15">
-                                          <Box className="row">
-                                              <Box className="form-item col-6 padd-15">
-                                                  <Box className="form-group">
-                                                      <input type="text" className="form-control" placeholder="Name" />
-                                                  </Box>
-                                              </Box>
-                                          </Box>
+                        <Box height={120} display={"flex"} marginRight={15} justifyContent={"center"} sx={{ gridColumn: "span 8" }}>
+                        <section className="contact section" >
+                            <Box className="row">
+                                    </Box>
+                                    <h3 className="contact-title padd-15">Got any questions?</h3>
+                                    <h4 className="contact-sub-title padd-15">WE ARE AT YOUR SERVICE</h4> 
+                            </section>
+                        
+                        </Box>
+                        <Box display={"flex"} justifyContent="right"  marginRight={1} sx={{ gridColumn: "span 1" }} marginTop={2}>
+                                <PersonOutlinedIcon fontSize="large" sx={{ color: colors.redAccent[500] }}/>
+                        </Box>
+                        <Box sx={{ gridColumn: "span 1" }}>
+                            <h4>Parth Patel</h4>
+                        </Box>
 
-                                          <Box className="row">
-                                              <Box className="form-item col-6 padd-15">
-                                                  <Box className="form-group">
-                                                      <input type="email" className="form-control" placeholder="Email" />
-                                                  </Box>
-                                              </Box>
-                                          </Box>
-                                              
-                                          <Box className="row">
-                                              <Box className="form-item col-12 padd-15">
-                                                  <Box className="form-group">
-                                                      <input type="text" className="form-control" placeholder="Subject" />
-                                                  </Box>
-                                              </Box>
-                                          </Box>
-                                              
-                                          <Box className="row">
-                                              <Box className="form-item col-12 padd-15">
-                                                  <Box className="form-group">
-                                                      <textarea name="" id="" className="form-control" placeholder="Message"></textarea>
-                                                  </Box>
-                                              </Box>
-                                          </Box>
-                                              
+                        <Box display={"flex"} justifyContent="right" marginRight={1} sx={{ gridColumn: "span 1" }} marginTop={2}>
+                            <LocalPhoneRoundedIcon fontSize="large" sx={{ color: colors.redAccent[500] }}/>
+                        </Box>
+                        <Box sx={{ gridColumn: "span 1" }}>
+                            <h4>Phone: +1(365) 688-8828</h4>
+                        </Box>
 
-                                          <Box className="row">
-                                              <Box className="form-item col-12 padd-15">
-                                                  <Box className="form-group">
-                                                      <button className="btn" type="submit">Send Message</button>
-                                                  </Box>
-                                              </Box>
-                                          </Box>
-                                      </Box>
-                                  </Box>
-                          </section>
+                        <Box display={"flex"} justifyContent="right" marginRight={1} sx={{ gridColumn: "span 1" }} marginTop={2}>
+                            <AttachEmailIcon fontSize="large" sx={{ color: colors.redAccent[500] }}/>
+                        </Box>
+                        <Box sx={{ gridColumn: "span 3" }}>
+                            <h4>Email : pypatel593m@gmail.com@gmail.com</h4>
+                        </Box>
 
-                      
-                      </Box>
-                      <Box sx={{ gridColumn: "span 8" }}>
 
-                      </Box>
+                        <Box display={"flex"} justifyContent="right"  marginRight={1} sx={{ gridColumn: "span 1" }} marginTop={2}>
+                                <PersonOutlinedIcon fontSize="large" sx={{ color: colors.redAccent[500] }}/>
+                        </Box>
+                        <Box sx={{ gridColumn: "span 1" }}>
+                            <h4>Dhruv Patel</h4>
+                        </Box>
+
+                        <Box display={"flex"} justifyContent="right" marginRight={1} sx={{ gridColumn: "span 1" }} marginTop={2}>
+                            <LocalPhoneRoundedIcon fontSize="large" sx={{ color: colors.redAccent[500] }}/>
+                        </Box>
+                        <Box sx={{ gridColumn: "span 1" }}>
+                            <h4>Phone: +1(365) 688-8828</h4>
+                        </Box>
+
+                        <Box display={"flex"} justifyContent="right" marginRight={1} sx={{ gridColumn: "span 1" }} marginTop={2}>
+                            <AttachEmailIcon fontSize="large" sx={{ color: colors.redAccent[500] }}/>
+                        </Box>
+                        <Box sx={{ gridColumn: "span 3" }}>
+                            <h4>Email : pypatel593m@gmail.com@gmail.com</h4>
+                        </Box>
+
+                        <Box display={"flex"} justifyContent="right"  marginRight={1} sx={{ gridColumn: "span 1" }} marginTop={2}>
+                                <PersonOutlinedIcon fontSize="large" sx={{ color: colors.redAccent[500] }}/>
+                        </Box>
+                        <Box sx={{ gridColumn: "span 1" }}>
+                            <h4>Arin Patodia</h4>
+                        </Box>
+
+                        <Box display={"flex"} justifyContent="right" marginRight={1} sx={{ gridColumn: "span 1" }} marginTop={2}>
+                            <LocalPhoneRoundedIcon fontSize="large" sx={{ color: colors.redAccent[500] }}/>
+                        </Box>
+                        <Box sx={{ gridColumn: "span 1" }}>
+                            <h4>Phone: +1(365) 688-8828</h4>
+                        </Box>
+
+                        <Box display={"flex"} justifyContent="right" marginRight={1} sx={{ gridColumn: "span 1" }} marginTop={2}>
+                            <AttachEmailIcon fontSize="large" sx={{ color: colors.redAccent[500] }}/>
+                        </Box>
+                        <Box sx={{ gridColumn: "span 3" }}>
+                            <h4>Email : pypatel593m@gmail.com@gmail.com</h4>
+                        </Box>
                       
                     </Box>
                   </Box>
