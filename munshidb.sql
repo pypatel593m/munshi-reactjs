17affc73cc1bb8a329f7168e5af9618fd8a40a2e
pypatel593m <pypatel593m@gmail.com>
2023-04-02 16:38:10 -0400

    added database script

--- munshidb.sql
+++ munshidb.sql
@@ -1,8 +1,64 @@
+CREATE TABLE user_types
+(
+    user_type_id SERIAL PRIMARY KEY,
+    user_type VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE users
+(
+    user_id SERIAL PRIMARY KEY,
+    user_email_address varchar(250) UNIQUE NOT NULL,
+    user_password varchar(120) NOT NULL,
+    user_fname varchar(255) NOT NULL,
+    user_lname varchar(255) NOT NULL,
+    user_phone character(12),
+    user_address varchar(255),
+    user_type_id INTEGER REFERENCES user_types(user_type_id)
+);
+
+CREATE TABLE businesses (
+   business_id integer UNIQUE,
+   business_name VARCHAR(250),
+   business_address VARCHAR(500),
+   business_phone CHAR(12)
+);
+
 CREATE TABLE positions (
   position_id SERIAL PRIMARY KEY,
   user_position VARCHAR(20),
   business_id INTEGER REFERENCES businesses(business_id)
 );
+
+CREATE TABLE schedules
+(
+    schedule_id SERIAL PRIMARY KEY,
+    schedule_date date NOT NULL,
+    shift_start_time time without time zone NOT NULL,
+    shift_end_time time without time zone NOT NULL,
+    notes varchar(2000),
+    user_id INTEGER REFERENCES users(user_id) NOT NULL,
+    position_id INTEGER REFERENCES positions(position_id) NOT NULL
+);
+
+CREATE TABLE shifts
+(
+    shift_id SERIAL PRIMARY KEY,
+    clock_in timestamp without time zone,
+    clock_out timestamp without time zone,
+    notes VARCHAR(2000),
+    schedule_id INTEGER REFERENCES schedules(schedule_id) NOT NULL,
+    user_id INTEGER REFERENCES users(user_id) NOT NULL
+)
+
+CREATE TABLE availabilities
+(
+    availability_id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(user_id) NOT NULL,
+    available_date date NOT NULL,
+    available_time_from time without time zone NOT NULL,
+    available_time_till time without time zone NOT NULL,
+    notes VARCHAR(2000)
+)
 
 CREATE TABLE teams (
   team_id SERIAL PRIMARY KEY,
@@ -16,12 +72,29 @@ CREATE TABLE team_members (
     wage DECIMAL(19,4)
 );
 
-CREATE TABLE businesses (
-   business_id integer UNIQUE,
-   business_name VARCHAR(250),
-   business_address VARCHAR(500),
-   business_phone CHAR(12)
-);
+CREATE TABLE breaks
+(
+    break_id SERIAL PRIMARY KEY,
+    break_length time without time zone NOT NULL,
+    is_paid boolean
+)
+
+CREATE TABLE public.breaks_list
+(
+    break_list_id SERIAL PRIMARY KEY,
+    break_start_time varchar(250),
+    break_end_time varchar(120),
+    shift_id INTEGER REFERENCES shifts(shift_id),
+    break_id INTEGER REFERENCES breaks(break_id)
+)
+
+CREATE TABLE chats
+(
+    chat_id SERIAL PRIMARY KEY,
+    senders_id INTEGER REFERENCES users(user_id) NOT NULL,
+    receivers_id INTEGER REFERENCES users(user_id) NOT NULL,
+    message_body varchar(2000)
+)
 
 
 CREATE OR REPLACE FUNCTION get_team_data(businessid integer)
