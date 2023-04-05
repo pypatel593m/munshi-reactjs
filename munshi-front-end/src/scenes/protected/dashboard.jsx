556c8a867ecbed539dbcb07a6d9376216b9884ea
pypatel593m <pypatel593m@gmail.com>
2023-04-05 00:05:36 -0400

    added some data on dashboard

--- munshi-front-end/src/scenes/protected/dashboard.jsx
+++ munshi-front-end/src/scenes/protected/dashboard.jsx
@@ -1,20 +1,55 @@
-import { Box } from "@mui/material"; 
+import { Box, Link, useTheme } from "@mui/material"; 
 import Header  from "../../components/Header";
-import {CheckLogin} from "../../util";
+import {CheckLogin, IsEmployer} from "../../util";
 import {useNavigate} from 'react-router-dom';
+import { tokens } from "../../theme";
 
 const Dashboard = () => {
   const navigate = useNavigate();
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
   
-  if (!CheckLogin)
-  {
-    navigate('/login');
-  }
   return <Box m="20px">
-      <Box display="flex" justifyContent="space-between" alignItems="center">
-        <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+    <Box display="flex" justifyContent="space-between" alignItems="center">
+      <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+    </Box>
+    { IsEmployer() === "employer" ? (
+      //This section is for employers 
+      <Box>
+        
+    <Box display="flex" justifyContent={"center"}>
+      <h3>Are you an employer, haven't registered your business yet, then <Link href="./business" color={colors.blueAccent[300]} className="register-link"> Click Here!</Link></h3>
+
       </Box>
-      <p></p>
+      <Box display="flex" justifyContent={"center"}>
+      <h3>Are you an employer, haven't registered your business yet, then <Link href="./business" color={colors.blueAccent[300]} className="register-link"> Click Here!</Link></h3>
+
+      </Box>
+      <Box display="flex" justifyContent={"center"}>
+      <h3>Are you an employer, haven't registered your business yet, then <Link href="./business" color={colors.blueAccent[300]} className="register-link"> Click Here!</Link></h3>
+
+      </Box>
+      </Box>
+    ): (
+      
+      // this section is for employees
+      <Box>
+        
+    <Box display="flex" justifyContent={"center"}>
+      <h3>Allllllllllllllllllllllllllllllllllllllllhen <Link href="./business" color={colors.blueAccent[300]} className="register-link"> Click Here!</Link></h3>
+
+      </Box>
+      <Box display="flex" justifyContent={"center"}>
+      <h3>Are you an employer, haven't registered your business yet, then <Link href="./business" color={colors.blueAccent[300]} className="register-link"> Click Here!</Link></h3>
+
+      </Box>
+      <Box display="flex" justifyContent={"center"}>
+      <h3>Are you an employer, haven't registered your business yet, then <Link href="./business" color={colors.blueAccent[300]} className="register-link"> Click Here!</Link></h3>
+
+      </Box>
+      </Box>
+    )}
+      
   </Box>;
     
 }
